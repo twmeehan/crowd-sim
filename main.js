d2c961e4580b16037e3dfac56ed73fd0aedea4be
Timothy <tmeehan26@gmail.com>
2025-04-13 05:33:42 -0400

    finished commenting my code

--- main.js
+++ main.js
@@ -1,37 +1,44 @@
 import { Vec3, Vec4, Mat4 , WEAVE} from './lib/weave-3d.esm.js'; 
 
-WEAVE.init();
 
-let camera = new WEAVE.Camera(WEAVE.Camera.PERSPECTIVE);
-WEAVE.setActiveCamera(camera);
+/*
+ * This is an simple example program to demonstrate weave-3d library. It will create
+ * a basic scene with a earth orbiting the sun while a moon orbits the earth. 
+ */
+
+WEAVE.init();
 
 let scene = new WEAVE.Scene();
 WEAVE.setActiveScene(scene);
 
+let camera = new WEAVE.Camera(WEAVE.Camera.PERSPECTIVE);
+WEAVE.setActiveCamera(camera);
 
+let earth = new WEAVE.Sphere();
+earth.mesh.color = new Vec4(0,0,1,0.5);
+earth.update = () => {
+    earth.transform.position = new Vec3(Math.sin(Date.now()/1000)*10,0,Math.cos(Date.now()/1000) * 7);
+    earth.dirty = true;
+};
 
-let box = new WEAVE.Box();
-scene.add(box);
+let moon = new WEAVE.Sphere();
+moon.transform.scale = new Vec3(0.3,0.3,0.3);
+moon.mesh.color = new Vec4(1,1,1,0.8);
+moon.update = () => {
+    moon.transform.position = new Vec3(Math.sin(Date.now()/300) * 2,0,Math.cos(Date.now()/300) *2);
+    moon.dirty = true;
+};
+earth.add(moon);
 
+let sun = new WEAVE.Box();
+sun.mesh.color = new Vec4(1,1,0,0.5);
 
+camera.update = () => {
+    camera.transform.position = new Vec3(Math.sin(Date.now()/5000) * 20,Math.cos(Date.now()/5000)*15,Math.cos(Date.now()/5000)* 20);
+    camera.lookAt(new Vec3(0,0,0));
+    camera.dirty = true;
+};
 
-let cube = new WEAVE.Box();
-scene.add(cube);
-cube.transform.position.x = 5;
-cube.add(camera);
-cube.mesh.color = new Vec4(1.0,0.0,0.0,1.0);
-cube.updateWorldMatrix();
-
-
-let sphere = new WEAVE.Sphere();
-box.add(sphere);
-sphere.transform.position.y = 2;
-sphere.updateWorldMatrix();
-sphere.mesh.color = new Vec4(0,0,1,0.3);
-
-camera.lookAt(new Vec3(0,0,0))
-
-
-WEAVE.render();
+WEAVE.start();
 
 
