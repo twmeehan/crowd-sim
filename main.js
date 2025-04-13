da5cbdf63a25d46f35f01e6e20a590d9032d991d
Timothy <tmeehan26@gmail.com>
2025-04-13 03:42:40 -0400

    weave-3d working

--- main.js
+++ main.js
@@ -8,8 +8,30 @@ WEAVE.setActiveCamera(camera);
 let scene = new WEAVE.Scene();
 WEAVE.setActiveScene(scene);
 
+
+
 let box = new WEAVE.Box();
 scene.add(box);
 
+
+
+let cube = new WEAVE.Box();
+scene.add(cube);
+cube.transform.position.x = 5;
+cube.add(camera);
+cube.mesh.color = new Vec4(1.0,0.0,0.0,1.0);
+cube.updateWorldMatrix();
+
+
+let sphere = new WEAVE.Sphere();
+box.add(sphere);
+sphere.transform.position.y = 2;
+sphere.updateWorldMatrix();
+sphere.mesh.color = new Vec4(0,0,1,0.3);
+
+camera.lookAt(new Vec3(0,0,0))
+
+
 WEAVE.render();
+
 
