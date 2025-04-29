2aeed105ebe6a66c0dc3ebb0220995adc8ffb1e4
Timothy <tmeehan26@gmail.com>
2025-04-29 01:16:14 -0400

    added OBJ/MTL loader and custom camera controls

--- main.js
+++ main.js
@@ -14,23 +14,27 @@ WEAVE.setActiveScene(scene);
 let camera = new WEAVE.Camera(WEAVE.Camera.PERSPECTIVE);
 WEAVE.setActiveCamera(camera);
 
-let sunLight = new WEAVE.DirectionalLight(new Vec3(-1,-1.3,-0.8),new Vec3(1,1,1),1);
-WEAVE.lights.push(sunLight);
+camera.controls = new WEAVE.CameraController(camera, canvas );
 
-
-let sun = new WEAVE.Sphere();
-sun.update = () => {
-    //sun.position = new Vec3(0,Math.cos(Date.now()/1000)*3,0);
-    sun.dirty = true;
-}
-
-sun.mesh.material = new WEAVE.Material(new Vec3(0,1,1),new Vec3(0.5,1,1),10)
-
-camera.update = () => {
 camera.position = new Vec3(Math.sin(Date.now()/1000) * 20,Math.cos(Date.now()/1000)*15,Math.cos(Date.now()/1000)* 20);
-    camera.lookAt(new Vec3(0,0,0));
-    camera.dirty = true;
-}
+camera.lookAt(new Vec3(0,0,0));
+camera.dirty = true;
+
+
+
+let sunLight = new WEAVE.DirectionalLight(new Vec3(-1,-1.3,-0.8),new Vec3(1,1,1),0.8);
+let ambientLight = new WEAVE.AmbientLight(new Vec3(1,1,1),0.3);
+WEAVE.lights.push(sunLight);
+WEAVE.lights.push(ambientLight);
+
+//let cannon = await WEAVE.Loader.loadObject('cart.obj','cart.mtl');
+
+let sun = new WEAVE.Box();
+sun.mesh.material =  (await WEAVE.Loader.loadMTL('cart.mtl'))['Material.002'];
+
+//sun.mesh.material = new WEAVE.Material(new Vec3(0,1,1),new Vec3(0.7,1,1),10)
+
+
 
 WEAVE.start();
 
